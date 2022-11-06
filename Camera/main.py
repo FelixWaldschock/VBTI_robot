a6d337acfd5b9e66bbae3ba9ea25d5ee089b0a4e
FelixW <felix.waldschock@hotmail.com>
2022-11-06 16:31:44 +0100

    Update Notebook

--- Camera/main.py
+++ Camera/main.py
@@ -2,3 +2,11 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pyrealsense2 as rs
+
+with rs.Service():
+    dev = rs.Device()
+
+    dev.wait_for_frame()
+    plt.imshow(dev.colour)
+    plt.show()
